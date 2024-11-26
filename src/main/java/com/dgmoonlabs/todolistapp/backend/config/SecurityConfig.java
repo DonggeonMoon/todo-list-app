96153971a6135901cfb2c739f568c4241e10f2e1
Donggeon Moon <nullsafer@naver.com>
2024-11-27 08:30:23 +0900

    feat: cors 설정 추가

--- src/main/java/com/dgmoonlabs/todolistapp/backend/config/SecurityConfig.java
+++ src/main/java/com/dgmoonlabs/todolistapp/backend/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -14,6 +18,16 @@ public class SecurityConfig {
         return http.authorizeHttpRequests(requests ->
                         requests.anyRequest().permitAll()
                 ).csrf(AbstractHttpConfigurer::disable)
+                .cors(configurer -> {
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                    corsConfiguration.setAllowedHeaders(List.of("*"));
+                    corsConfiguration.setAllowCredentials(true);
+                    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                    source.registerCorsConfiguration("/**", corsConfiguration);
+                    configurer.configurationSource(source);
+                })
                 .build();
     }
 }
